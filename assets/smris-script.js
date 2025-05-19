5a17a50b9a2fe336b49e5bc6a0d4a9e18f7f7e82
harun-prasad <harunprasad@gmail.com>
2025-05-19 14:58:23 +0530

    Theme Exported from Shopify theme_export__smris-com-smris-expanse-theme__19MAY2025-0138pm.zip

--- assets/smris-script.js
+++ assets/smris-script.js
@@ -1,51 +1,20 @@
 // Scripts
+  
+  // Disable right-click on the entire page
+  document.addEventListener('contextmenu', function (event) {
+    event.preventDefault();
+  });
 
- window.oncontextmenu = function () {
+  // Prevent image dragging
+  document.addEventListener('dragstart', function (event) {
+    if (event.target.tagName === 'IMG') {
+      event.preventDefault();
+    }
+  });
+
+  // Disable image download via long-press (for mobile)
+  document.querySelectorAll('img').forEach(function (img) {
+    img.style.pointerEvents = 'none';
+  });
  
-  // Disable Right Click
-  document.ondragstart = function () {
-    return false;
-  }
-
-  function nocontext(e) {
-    return false;
-  }
-  document.oncontextmenu = nocontext;
-
-
-  // Disable View Source
-  document.onkeydown = function (e) {
-    if (e.ctrlKey
-      && (e.keyCode === 67
-        || e.keyCode === 86
-        || e.keyCode === 85
-        || e.keyCode === 117)) {
-      return false;
-    } else {
-      return true;
-    }
-  };
-
-  $(document).keypress("u", function (e) {
-    if (e.ctrlKey) {
-      return false;
-    } else {
-      return true;
-    }
-  });
-
-  // Disable F12 Key
-  $(document).keydown(function (event) {
-    if (event.keyCode == 123) {
-      return false;
-    } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) { // Prevent Ctrl+Shift+I        
-      return false;
-    }
-  });
-
-  $(document).on("contextmenu", function (e) {
-    e.preventDefault();
-  });
-
-}
 // End Script
